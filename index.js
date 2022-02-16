2e97ef4aeb208d4bd2c40b8fa986832a13a4b66f
Copy <71971770+Copy05@users.noreply.github.com>
2022-02-16 12:55:40 +0100

    Core Files Uploaded

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { TOKEN } = require('./config.json')
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+// The Bot goes up
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction, client);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// Logins into the bot account
+client.login(TOKEN);
